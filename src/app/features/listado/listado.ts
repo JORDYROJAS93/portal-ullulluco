c63d5c3cd2d95d9da5f971ad71b8fa893173b09f
JORDYROJAS93 <jordy_r12@hotmail.com>
2026-05-26 15:26:23 -0500

    cambiar categoria turismo por paisajes

--- src/app/features/listado/listado.ts
+++ src/app/features/listado/listado.ts
@@ -67,7 +67,7 @@ export class ListadoComponent implements OnInit {
       historia: 'Historia de Alfonso Ugarte',
       festividades: 'Festividades y Costumbres',
       agricultura: 'Nuestra Agricultura',
-      turismo: 'Paisajes en nuestro Pueblo',
+      turismo: 'Paisajes en nuestra Tierra',
       leyendas: 'Mitos y Leyendas',
       'plantas-nativas': 'Flora Nativa',
       autoridades: 'Nuestras Autoridades',
